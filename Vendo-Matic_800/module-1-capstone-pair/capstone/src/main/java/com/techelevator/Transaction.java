857a24d1dab1df68f85c4a121a36237a57707793
granthamj88 <64680033+granthamj88@users.noreply.github.com>
2022-09-16 14:05:05 -0700

    Add files via upload

---
+++ Vendo-Matic_800/module-1-capstone-pair/capstone/src/main/java/com/techelevator/Transaction.java
@@ -0,0 +1,125 @@
+package com.techelevator;
+
+import javax.crypto.Mac;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Transaction {
+    public static String balancemessage = "Current Money Provided: $";
+    public static BigDecimal balance = new BigDecimal(0.00).setScale(2);
+    private static BigDecimal pastBalance = balance;
+    private static int quarters;
+    private static int dimes;
+    private static int nickels;
+    private static double balanceD;
+    private static final Queue receipt = new LinkedList<String>();
+
+    //returns the balance
+    public static BigDecimal getBalance() {
+        return Transaction.balance;
+    }
+
+    //method to feed money into the vending machine
+    public static void feedCash(String feed) throws FileNotFoundException {
+        if(feed.equals("$1")) {
+            balance = balance.add(new BigDecimal(1.00));
+        } else if(feed.equals("$2")) {
+            balance = balance.add(new BigDecimal(2.00));
+        } else if(feed.equals("$5")) {
+            balance = balance.add(new BigDecimal(5.00));
+        } else if(feed.equals("$10")) {
+            balance = balance.add(new BigDecimal(10.00));
+        }
+        System.out.println("Current Money Provided: $" + balance);
+        //updates audit log with money feed
+        writeAuditLog("FEED MONEY", pastBalance, balance);
+    }
+
+    //method to make change when asked
+    public static void makeChange(BigDecimal balance) throws FileNotFoundException {
+        BigDecimal oldBalance = balance;
+        balanceD = (balance.doubleValue()*100);
+        quarters = (int) (balanceD/25);
+        balanceD -= (quarters*25);
+        dimes = (int) (balanceD/10);
+        balanceD -= (dimes*10);
+        nickels = (int) balanceD/5;
+        Transaction.balance = balance.multiply(new BigDecimal(0.00));
+        oldBalance = oldBalance.multiply(new BigDecimal(0.00));
+        System.out.println("Your Change: " + quarters + " quarters " + dimes + " dimes " + nickels + " nickels.");
+        //updates audit log with change
+        writeAuditLog("GIVE CHANGE", oldBalance, balance);
+    }
+
+    //the method below will allow customer to buy item
+
+    public static void buySnacks(String snackCode) throws FileNotFoundException {
+        //when the code doesn't match, it'll come out invalid
+        if(!MachineItems.itemCode.containsKey(snackCode)) {
+            System.out.println("Code Invalid, please try a valid code");
+        } else if (MachineItems.itemCode.containsKey(snackCode)) {
+            //checking if the balance the customer has provided is enough
+            if(balance.compareTo(MachineItems.itemPrice.get(snackCode)) == -1) {
+                System.out.println("INSUFFICIENT FUNDS");
+            } else if(MachineItems.itemStock.get(snackCode) == 0) {
+                //checks if item is out of stock
+                System.out.println("OUT OF STOCK");
+            } else if(balance.compareTo(MachineItems.itemPrice.get(snackCode)) == 1 || balance.compareTo(MachineItems.itemPrice.get(snackCode)) == 0) {
+                //the balance will update
+                BigDecimal oldBalance = balance;
+                //adds customers bought items to a receipt
+                receipt.add(MachineItems.itemName.get(snackCode) +" - $"+ MachineItems.itemPrice.get(snackCode));
+                balance = balance.subtract(MachineItems.itemPrice.get(snackCode));
+                //taking from the stock
+                MachineItems.itemStock.put(snackCode, MachineItems.itemStock.get(snackCode)-1);
+                writeAuditLog(MachineItems.itemName.get(snackCode), oldBalance, balance);
+                System.out.println("Enjoy! \n" + "Your balance is now: $" + balance);
+                //printing out the messages based on codes
+                if(snackCode.contains("A")) {
+                    Chips.getMessage();
+                } else if (snackCode.contains("B")) {
+                    Candy.getMessage();
+                } else if(snackCode.contains("C")) {
+                    Beverage.getMessage();
+                } else if(snackCode.contains("D")) {
+                    Gum.getMessage();
+                }
+            }
+
+        }
+    }
+
+    //method to call the receipt list and print it out to the customer
+    public static void getReceipt() throws FileNotFoundException{
+        if (receipt.size() != 0) {
+            Double total = 0.0;
+            System.out.println(System.lineSeparator() + "***Receipt Print***");
+            for (Object f : receipt) {
+                total += Double.parseDouble(f.toString().substring(f.toString().length() - 4));
+
+                System.out.println(f);
+
+            }
+            System.out.println(System.lineSeparator() + "Items bought - " + receipt.size() + "\nTotal Spent - $" + String.format("%.2f", total));
+            System.out.println("Thank you! Come again!!!");
+
+        }
+    }
+
+    //log to audit all the transactions that are being done
+    public static void writeAuditLog(String transaction, BigDecimal oldBalance, BigDecimal currentBalance) throws FileNotFoundException {
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter specificFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a");
+        String dateFormatted = dateTime.format(specificFormat);
+        try(PrintWriter auditLog = new PrintWriter(new FileOutputStream("AuditLog.txt", true))) {
+            auditLog.println(">" + dateFormatted + " " + transaction + " $" + oldBalance + " $" + currentBalance);
+        }
+    }
+}
---
+++ Vendo-Matic_800/module-1-capstone-pair/capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -0,0 +1,121 @@
+package com.techelevator;
+
+import com.sun.nio.sctp.AbstractNotificationHandler;
+import com.techelevator.view.Menu;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
+
+import static com.techelevator.MachineItems.*;
+
+public class VendingMachineCLI {
+
+	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
+	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
+	private static final String[] BACK_BUTTON = {"Back"};
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, "Exit"};
+	private static final String[] PURCHASE_MENU_OPTIONS = {"Feed Money", "Select Product", "Get Change", "Finish Transaction"};
+	private static  final String[] CASH_INPUT_OPTIONS = {"$1", "$2", "$5", "$10", "Back"};
+
+
+
+
+	private Menu menu;
+
+	public VendingMachineCLI(Menu menu) {
+		this.menu = menu;
+	}
+	//7 - display vending machine items from file and with map
+	public void productMenu() {
+		for(String f : itemCode.keySet()) {
+			if (itemStock.get(f) == 0) {
+				System.out.println(f + "| " + itemName.get(f) + "| $" + itemPrice.get(f) + "| SOLD OUT");
+			} else {
+				System.out.println(f + "| " + itemName.get(f) + "| $" + itemPrice.get(f) + "| " + itemStock.get(f) + " in stock");
+			}
+		}
+		//only show the back button and not the whole main menu
+		String choice = (String) menu.getChoiceFromOptions(BACK_BUTTON);
+	}
+
+
+	public void run() throws FileNotFoundException {
+		//5- running the vending machine file
+		MachineItems.readFile();
+		while (true) {
+			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
+
+			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+				//displaying the product menu
+				productMenu();
+			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+				//8 - do the purchase
+				purchaseMenu();
+			} else if (choice.equals("Exit")) {
+				//6 - added exit code
+				System.exit(3);
+			}
+		}
+	}
+
+	//10- Method to run the purchase menu
+	private void purchaseMenu() throws FileNotFoundException {
+		String purchaseOption = "";
+		while (!purchaseOption.contentEquals("Finish Transaction")) {
+			purchaseOption = (String) menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
+			if (purchaseOption.contentEquals("Feed Money")) {
+				customerMoneyFeed();
+			} else if (purchaseOption.contentEquals("Select Product")) {
+				productSelection();
+			} else if (purchaseOption.contentEquals("Get Change")) {
+				Transaction.makeChange(Transaction.getBalance());
+			}
+		}
+		if (purchaseOption.contentEquals("Finish Transaction")) {
+			Transaction.getReceipt();
+			Transaction.makeChange(Transaction.getBalance());
+		}
+	}
+
+	//giving the customer their money options
+	private void customerMoneyFeed() throws FileNotFoundException {
+		String moneyOptions = "";
+		while(!moneyOptions.contentEquals("Back")) {
+			moneyOptions = (String) menu.getChoiceFromOptions(CASH_INPUT_OPTIONS);
+			System.out.println(moneyOptions);
+			Transaction.feedCash(moneyOptions);
+		}
+	}
+
+	//user input product selection from menu
+	private void productSelection() throws FileNotFoundException {
+		Set<String> itemCodeKeys = itemCode.keySet();
+		for (String key : itemCodeKeys) {
+			if (itemStock.get(key) == 0) {
+				System.out.println(key + "| " + itemName.get(key) + "| $" + itemPrice.get(key) + "| OUT OF STOCK");
+			} else {
+				System.out.println(key + "| " + itemName.get(key) + "| $" + itemPrice.get(key) + "| " + itemStock.get(key) + " in stock");
+			}
+		}
+		System.out.println("\n1) Back" + "\nCurrent Money Provided: $" + Transaction.getBalance() + "\n" + "\nPlease Make a Selection >>> ");
+		Scanner userInput = new Scanner(System.in);
+		String itemCode = userInput.nextLine().toUpperCase();
+		//back button for the user
+		if (itemCode.equals("1")) {
+			purchaseMenu();
+		} else {
+			//transaction
+			Transaction.buySnacks(itemCode);
+		}
+	}
+
+	public static void main(String[] args) throws FileNotFoundException{
+		Menu menu = new Menu(System.in, System.out);
+		VendingMachineCLI cli = new VendingMachineCLI(menu);
+		cli.run();
+	}
+}
